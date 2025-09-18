c18e92adf2ea2b942e95c2be30cb0d443c61ee16
Leonardo <leonardosantosbaia@gmail.com>
2025-09-18 03:33:57 -0300

    Corrige estrutura de pastas e planeja novo layout da tela inicial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
+     backgroundColor: '#FFFFFF',
   },
   title: {
     fontSize: 24,
